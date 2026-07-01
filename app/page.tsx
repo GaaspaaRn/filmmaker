242dc6b7980fc5ff497cc09cfd9814883df3742c
GaaspaaRn <pablogasparsilva@gmail.com>
2026-07-01 18:31:43 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -154,10 +154,10 @@ export default function Page() {
         <div className="flex flex-col items-center text-center gap-4 max-w-4xl w-full pointer-events-auto">
           {/* Socials */}
           <div className="flex items-center gap-6 text-white mix-blend-difference">
-            <a href="https://www.instagram.com/cauahcm/" target="_blank" rel="noopener noreferrer" className="hover:opacity-70 transition-opacity duration-300">
+            <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" className="hover:opacity-70 transition-opacity duration-300">
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/></svg>
             </a>
-            <a href="https://www.behance.net/cauahcm" target="_blank" rel="noopener noreferrer" className="hover:opacity-70 transition-opacity duration-300">
+            <a href="https://www.behance.net/" target="_blank" rel="noopener noreferrer" className="hover:opacity-70 transition-opacity duration-300">
               <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="currentColor"><path d="M22 7h-7v-2h7v2zm1.726 10c-.442 1.297-2.029 3-5.101 3-3.074 0-5.564-1.729-5.564-5.675 0-3.91 2.325-5.92 5.466-5.92 3.082 0 4.964 1.782 5.375 4.426.078.506.109 1.188.095 2.14h-8.027c.13 3.211 3.483 3.312 4.588 2.029h3.168zm-7.686-4h4.965c-.105-1.547-1.136-2.219-2.477-2.219-1.466 0-2.277.768-2.488 2.219zm-9.574 6.988h-6.466v-14.967h6.953c5.476.081 5.58 5.444 2.72 6.906 3.461 1.26 3.577 8.061-3.207 8.061zm-3.466-8.988h3.584c2.508 0 2.906-3-.312-3h-3.272v3zm3.391 3h-3.391v3.016h3.341c3.055 0 2.868-3.016.05-3.016z"/></svg>
             </a>
             <a href="https://api.whatsapp.com/send/?phone=5547996938902&text=Ol%C3%A1%2C+vim+pelo+site%3F&type=phone_number&app_absent=0" target="_blank" rel="noopener noreferrer" className="hover:opacity-70 transition-opacity duration-300">
